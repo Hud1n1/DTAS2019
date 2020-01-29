1a40b1e17130efcfb714da2ee2f16e1484ba353a
przand1 <przand1@st.amu.edu.pl>
2020-01-29 14:23:51 +0100

    update search comp

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -23,7 +23,8 @@ export class Search extends Component {
       this.state = {
         bookStats: [],
         pageNr: 0,
-        searchTerm: ''
+        searchTerm: '',
+          foundCount: 0
       };
     }
 
@@ -36,6 +37,10 @@ export class Search extends Component {
         <td>{status.book.author}</td>
         <td>{status.book.year}</td>
         <td>{status.book.category}</td>
+          <td>{status.rented ? `Wypożyczna do ${status.rentedUntil.toString().slice(0,10)}` : 'Tak'}</td>
+          <td>
+              <Rental status={status}/>
+          </td>
       </tr>)
     }
 
@@ -48,38 +53,50 @@ export class Search extends Component {
         console.log('submit', this.state.pageNr)
     }
      nextPage = event => {
-      this.doSearch(this.state.searchTerm, this.state.pageNr + 1)
-     this.state.pageNr = this.state.pageNr + 1;
-      console.log('next', this.state.pageNr)
-    }
+        this.doSearch(this.state.searchTerm, this.state.pageNr + 1);
+        this.state.pageNr = this.state.pageNr + 1;
+        console.log('next', this.state.pageNr)
+    };
      prevPage = event => {
       event.preventDefault();
       if(this.state.pageNr > 0) {
-        this.doSearch(this.state.searchTerm, this.state.pageNr - 1);
+          this.doSearch(this.state.searchTerm, this.state.pageNr - 1);
           this.state.pageNr = this.state.pageNr - 1;
-        console.log('prevvvvvv', this.state.pageNr)
+          console.log('prevvvvvv', this.state.pageNr)
       }
-    }
+    };
 
     async doSearch(term, pageNr) {
         //console.log(`Search term: ${term}`);
 
         console.log(term,pageNr);
-        console.log(`http://localhost:8080/api/bookstatus/find/${pageNr}?size=5&title=${term}`);
+
         this.setState({
-          bookStats: await Axios.get(`http://localhost:8080/api/bookstatus/find/${pageNr}?size=2&title=${term}`)
-          .then(function(response){
-            console.log(`Api response:`,response);
-            if (response.data.length === 0) {
-                window.alert('Brak wyników')
-            }
-            return response.data;
-          })
+              bookStats: await Axios.get(`http://localhost:8080/api/bookstatus/find/${pageNr}?size=2&title=${term}`)
+              .then(function(response){
+                console.log(`Api response:`,response);
+                if (response.data.length === 0) {
+                    window.alert('Brak wyników. Naciśnij "Wstecz" by wrócić.');
+                }
+                return response.data;
+              }),
+            foundCount: await Axios.get(`http://localhost:8080/api/bookstatus/count?title=${term}`)
+                .then(function (response) {
+                    console.log('bookcount:',response);
+                    return response.data;
+                })
         });
-    
+
     }
-          
-    
+
+    BookCount = (props) => {
+        if(props.count) {
+            return <div>Znalezionych pozycji: {props.count}</div>
+        }
+        return <div/>
+    };
+
+
   render() {
     
     return(
@@ -97,7 +114,9 @@ export class Search extends Component {
             </Form>
           </ToastBody>
         </Toast>
-      
+
+      <this.BookCount count={this.state.foundCount}/>
+
       <Table responsive>
         <thead>
           <tr>
@@ -106,6 +125,8 @@ export class Search extends Component {
             <th>Autor</th>
             <th>Data wydania</th>
             <th>Dział</th>
+              <th>Dostępność</th>
+            <th>Akcje</th>
           </tr>
         </thead>
        
